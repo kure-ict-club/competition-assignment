f1f67306e9a6d93d423809e4b5ab3453e8afe283
srpkdyy <leader931334@outlook.jp>
2018-03-21 04:21:22 +0900

    [update] implemented bubble sort

--- srpkdyy/q3/main.cpp
+++ srpkdyy/q3/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 class Text {
 private:
@@ -9,22 +11,65 @@ private:
    std::vector<std::string> text;
 
 public:
-   Text() : lineNum(0) {}
-   void readFromFile(std::ifstream& readFile) {
-      std::string tmp;
-      while (getline(readFile, tmp)) {
-         text.push_back(tmp);
+   Text(std::ifstream& readFile) : lineNum(0) {
+      std::string buf;
+      while (getline(readFile, buf)) {
+         text.push_back(buf);
          lineNum++;
       }
    }
+
    int lineCount() const {
       return lineNum;
    }
+
    std::string getStr(int line) {
       return text[line];
    }
+
    char getChar(int line, int row) {
       return text[line][row];
+   }
+};
+
+class Rearranger {
+public:
+   std::vector<int> sortData;
+
+public:
+   Rearranger(std::string data, char split) {
+      std::stringstream ss { data };
+      std::string buf;
+      while(getline(ss, buf, split)) sortData.push_back(std::stoi(buf));
+   }
+
+   int size() const {
+      return sortData.size();
+   }
+
+   void bubbleSort() {
+      for (int i=0; i<sortData.size()-1; ++i) {
+         bool hasSorted = false;
+
+         for (int j=0; j<sortData.size()-1-i; ++j) {
+            if (sortData[j] > sortData[j+1]) {
+               std::swap(sortData[j], sortData[j+1]);
+               hasSorted = true;
+            }
+         }
+
+         if (hasSorted == false) return;
+      }
+   }
+
+   std::vector<int> getDescenging() const {
+      return sortData;
+   }
+
+   std::vector<int> getAscending() const {
+      auto data = sortData;
+      std::reverse(data.begin(), data.end());
+      return data;
    }
 };
 
@@ -33,14 +78,16 @@ int main() {
    std::ifstream ifs("inputdata.txt");
    if (ifs.fail()) return -1;
 
-   Text readData;
-   readData.readFromFile(ifs);
+   Text text(ifs);
 
-   for (int i=0; i<readData.lineCount(); ++i) {
-      std::cout << readData.getStr(i) << std::endl;
+   Rearranger data(text.getStr(2), ' ');
+
+   data.bubbleSort();
+   //rear.selectSort();
+
+   for (auto value : data.getAscending()) {
+      std::cout << value << std::endl;
    }
-
-   std::cout << readData.getChar(0, 1) << std::endl;
 
 	return 0;
 }
